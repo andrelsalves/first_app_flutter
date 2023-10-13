fc5f0793a9c6ce3b0800e21ffc944b86b5826aa6
andrelsalves <andreluizalves54@live.com>
2023-10-13 12:14:37 -0300

    Adiconando processo de criacao de conta e login

--- lib/screens/authTela.dart
+++ lib/screens/authTela.dart
@@ -10,7 +10,7 @@ class AuthTela extends StatefulWidget {
 
 class _AuthTelaState extends State<AuthTela> {
   bool wantSign = true;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,26 +25,85 @@ class _AuthTelaState extends State<AuthTela> {
                 colors:[ 
                 MyColors.blueTopGradiente,
                 MyColors.blueBottomGradiente,
-            ])),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset("assets/Imagem1.png", height: 118,),
-              const Text(
-                "RCSB Consulting",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                ),
-              ),
             ],
           ),
-        ],
+        ),
       ),
-    );
-  }
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset("assets/Imagem1.png", height: 128,),
+                    const Text(
+                      "RCSB Consulting",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("E-mail"),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Senha"),
+                      ),
+                      obscureText: true,
+                    ),
+                    Visibility(
+                      visible: !wantSign,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              label: Text("Confirme a Senha"),
+                            ),
+                            obscureText: true,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              label: Text("Nome"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16
+                    ),
+                    ElevatedButton(
+                      onPressed: (){},
+                      child: Text((wantSign)? "Sign" : "Sign Up"),
+                    ),
+                    const Divider(),
+                      TextButton(onPressed:() {
+                        setState(() {
+                          wantSign = !wantSign;
+                        }
+                      );
+                    },
+                    child: Text(
+                      (wantSign)? "Don't Have a Account? Sign Up" : "Have a Account? Sign In"),
+                    ),
+                  ],
+                ),
+            ),
+          ),
+          ),
+      ),
+      ],
+    ),
+  );
+}
 }
